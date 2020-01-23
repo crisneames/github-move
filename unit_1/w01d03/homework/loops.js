9f3e265270c205345b48ec4876fb75b5d8562993
Cristi Neames <crisneames@cristis-imac.attlocal.net>
2020-01-22 21:39:04 -0600

    finished loop practice

---
+++ unit_1/w01d03/homework/loops.js
@@ -0,0 +1,19 @@
+// for (var i = 0; i <= 99; i++) {
+//   console.log(i)
+// };
+
+// for (var i = 0; i <= 98; i += 2) {
+//   console.log(i)
+// };
+
+// for (var i = 1; i <= 99; i+= 2) {
+//   console.log(i)
+// };
+
+// for (var i = 49; i <= 72; i++ ) {
+//   console.log(i)
+// };
+
+for (var i = 3; i <= 90; i += 3 ) {
+  console.log(i)
+};
