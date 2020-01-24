2e7561e7b3d3b1252eb203f5cb8dfe773aaf0317
Cristi Neames <crisneames@cristis-imac.attlocal.net>
2020-01-23 19:57:54 -0600

    yell at the ninja turtles answered

--- unit_1/w01d04/homework/answers.js
+++ unit_1/w01d04/homework/answers.js
@@ -24,15 +24,44 @@
 
 //Fizz Buzz
 
-let aNum = 5;
-for (let i = 0 ; i <= 100; i++) {
-if (aNum % 3 === 0)
-    console.log("Fizz")
-} else if {
-  (aNum % 5 === 0)
-  console.log('Buzz')
-} else if {
-  (aNum % 3 || aNum % 5) {
-    console.log('FizzBuzz')
-  }
+// let aNum = 15;
+// for (let i = 0 ; i <= 100; i++) {
+// if (aNum % 3 || aNum % 5) {
+//   console.log('FizzBuzz')
+// } else if (aNum % 3 === 0) {
+//     console.log("Fizz")
+//   } else if  (aNum % 5 === 0) {
+//     console.log('Buzz')
+//   } else if (aNum % 3 || aNum % 5) {
+//     console.log('FizzBuzz')
+//     }
+//   };
+
+//Wild Wild Life
+//
+const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
+const sharky = ["Sharky", "shark", 20, "Left Coast"]
+const plantee = ["Plantee", "plant",  5000 , "Mordor"]
+const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
+const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"]
+
+// //1.
+// plantee[2] = 5001;
+// //2.
+// wolfy[3] = 'Yukon Territory';
+// //3.
+// //*****Not sure about this one******
+// //4.
+// wolfy[0] = 'Gameboy';
+//
+// console.log(wolfy);
+// console.log(plantee);
+
+//Yell at the Ninja Turtles
+//
+let turtles = ['Donatello', 'Leonardo', 'Raphael', 'Michaelangelo'];
+let x;
+
+for (x of turtles) {
+  console.log(x)
 };
