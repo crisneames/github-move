197e7924565932872954f156d9cbf4912155120d
Cristi Neames <crisneames.gmail.com>
2020-01-28 18:45:52 -0600

    took it easy

---
+++ unit_1/w02d02/homework/answers.js
@@ -0,0 +1,38 @@
+//////
+// Answer the following
+//////
+
+// A light switch that can be either on or off. Boolean
+// A user's email address. String
+// A spaceship with a hull, laser blasters, tractor beam, and warp drive. Array
+// A list of student names from our class. Array
+// A list of student names from our class, each with a location. Object
+// A list of student names from our class, each with a location and each with a list of favorite tv shows. Object with an array
+
+//////
+// Take it Easy
+//////
+
+// Make an array that holds all of the colors of the rainbow.
+
+const rainbowColors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+console.log(rainbowColors);
+
+// Write code that will access "blue" from the rainbow array.
+
+console.log(rainbowColors[4]);
+
+// Make an object that is called your name and holds the information about your
+//favorite food, a hobby, the name of the town that you live in currently, and your favorite
+//datatype.
+
+const cristi = {
+favoriteFood: 'hamburger',
+hobby: 'photography',
+town: 'Tylertown',
+favoriteDatatype: 'Boolean'
+}
+console.log(cristi);
+// Write code that will access your hobby from the object that you just created.
+
+console.log(cristi.hobby);
