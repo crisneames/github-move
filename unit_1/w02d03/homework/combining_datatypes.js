a539003689ebabe9fc9028d86dc300cdf21d59cb
Cristi Neames <crisneames.gmail.com>
2020-01-29 19:30:41 -0600

    combine objects arrays and functions more than one level deep

--- unit_1/w02d03/homework/combining_datatypes.js
+++ unit_1/w02d03/homework/combining_datatypes.js
@@ -35,3 +35,28 @@ const apartmentBuilding = [
 ]
 //console.log(apartmentBuilding[0].tennents)[0];
 //////////////////can't get this one
+
+
+//Combine objects, arrays, and functions more than one level deep
+
+// 1.
+const knit = () => {
+  return  {item: 'scarf', size: '6 ft'}
+}
+console.log(knit().item);
+
+// 2.
+const crayonSelector = () => {
+return {box: 'crayons',
+        crayons: ['red', 'green', 'yellow', 'blue', 'orange']
+        }
+}
+console.log(crayonSelector().crayons[2]);
+
+// 3.
+const powerButton = () => {
+  return options () ==> {
+    conole.log('select a song'}
+
+};
+powerButton();
