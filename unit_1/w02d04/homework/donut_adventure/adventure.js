e4761fb3dc3120958288c02a30b0be4e12faa08b
Cristi Neames <crisneames.gmail.com>
2020-01-30 21:01:07 -0600

    created our enemy pizza rat

--- unit_1/w02d04/homework/donut_adventure/adventure.js
+++ unit_1/w02d04/homework/donut_adventure/adventure.js
@@ -30,3 +30,27 @@ constructor(name, health = 100){
 const dougie = new Hero('Dougie the Donut', 100);
 console.log(dougie)
 console.log(dougie.catchPhrases)
+
+class Enemy {
+  constructor(name, health = 100){
+    this.name = name;
+    this.health = health;
+    this.weapons = {
+      pepperoniStars: 5,
+      cheeseGrease: 10
+    }
+    }
+    catchPhrases = ['I\'m Youtube famnous', 'I\'m more dangerous that an uncovered sewer']
+    talkSmack () {
+      console.log(catchPhrases[Math.floor(Math.random() * catchPhrases.length)]);
+    }
+    announceHealth () {
+      console.log(this.health);
+    }
+    fight () {
+      console.log('I\'m gonna flatten you like a slice of pepperoni!');
+    }
+}
+
+const pizzaRat = new Enemy('Pizza Rat')
+console.log(pizzaRat)
