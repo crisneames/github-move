d5a7d0f198a950672c335f90d3528467f564e733
Cristi Neames <crisneames.gmail.com>
2020-02-03 17:13:47 -0600

    i added my first element and it was fun

--- unit_1/w03d01/homework/hogwarts/app.js
+++ unit_1/w03d01/homework/hogwarts/app.js
@@ -1,4 +1,19 @@
 // if (typeof jQuery == 'undefined'){
 //   console.log('oops! I still have to link my jQuery properly!');
 // } else {console.log('I did it! I linked jQuery and this js file!')};
-console.log('Homework');
+// console.log('Homework');
+
+$(() => {
+	//Year 1
+  //
+  //Query div with the id of container
+  const $container = $('#container')
+  console.log($container);
+// creates a new h1 tag
+const $h1 = $("<h1>").text("Hogwarts");
+$($h1).appendTo('body')
+console.log($h1);
+
+
+
+});
