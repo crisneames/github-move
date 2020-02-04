9baf905e3ef37ecb0d3dae0b580907ac8cd8d777
Cristi Neames <crisneames.gmail.com>
2020-02-03 18:14:49 -0600

    I added more elements. My power grows with my dedication and hard work

--- unit_1/w03d01/homework/hogwarts/app.js
+++ unit_1/w03d01/homework/hogwarts/app.js
@@ -8,11 +8,32 @@ $(() => {
   //
   //Query div with the id of container
   const $container = $('#container')
-  console.log($container);
-// creates a new h1 tag
+  //console.log($container);
+// creates a new h1 tag and add text
 const $h1 = $("<h1>").text("Hogwarts");
-$($h1).appendTo('body')
-console.log($h1);
+// append the h1 tag to the body
+$("body").append($h1);
+//console.log($h1);
+
+// Year 2
+//
+//Name - addd an h2 with the text Snowy
+const $name = $("<h2>").text("Snowy");
+// House - add an h3 with the text Ravenclaw
+const $house = $("<h3>").text("Ravenclaw");
+// Pet - add an h4 tag with the text Jo
+const $pet = $("<h4>").text('Jo');
+// add the class of owl to h4 pet
+$pet.addClass("owl");
+//wand - add an h4 with the text Wizzy
+const $wand = $("<h4>").text("Wizzy");
+// Append each to the body
+$($name).appendTo("body");
+$($house).appendTo("body");
+$($pet).appendTo("body");
+$($wand).appendTo('body');
+//console.log($name, $pet, $wand, $house);
+
 
 
 
