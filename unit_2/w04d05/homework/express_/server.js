f88f0a868d36755ae5d713197406e8775894a548
Cristi Neames <crisneames.gmail.com>
2020-02-15 15:06:35 -0600

    magic 8 express application created

--- unit_2/w04d05/homework/express_/server.js
+++ unit_2/w04d05/homework/express_/server.js
@@ -28,3 +28,13 @@ app.get('/tip/:total/:tipPercentage', (req, res) => {
 });
 
 //Magic 8 Ball
+
+app.get('/magic/:question', (req, res) => {
+  const answers = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
+
+  let question = req.params.question
+  //console.log(question)
+  let i = Math.floor(Math.random() * answers.length - 1);
+  //console.log(i);
+  res.send(`${question} : ${answers[i]}`);
+});
