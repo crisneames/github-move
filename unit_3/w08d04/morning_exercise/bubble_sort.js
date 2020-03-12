a6e38e023d1cfb8a70fbe5ecc98511741c885969
Cristi Neames <crisneames.gmail.com>
2020-03-12 14:18:11 -0500

    back end commit of holiday proj

---
+++ unit_3/w08d04/morning_exercise/bubble_sort.js
@@ -0,0 +1,20 @@
+const arr = [4, 3, 6, 77, 0]
+
+const bubbleSort = arr => {
+  let swapped;
+  do {
+    swapped = false;
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] > arr[i + 1]) {
+        let tmp = arr[i];
+        arr[i] = arr[i + 1];
+        arr[i + 1] = tmp;
+        swapped = true;
+      }
+    }
+  } while (swapped);
+  return arr;
+};
+
+bubbleSort(arr);
+console.log(bubbleSort(arr));
---
+++ unit_3/w08d04/student_examples/holidays/holidays_back_end/controllers/holidays.js
@@ -0,0 +1,45 @@
+const holidays = require('express').Router()
+const Holiday = require('../models/holidays.js')
+
+// Index route
+holidays.get('/', (req, res) => {
+  Holiday.find({}, (err, foundHolidays) => {
+    if (err) {
+      res.status(400).json({ error: err.message })
+    }
+    res.status(200).json(foundHolidays)
+  })
+})
+
+
+// Create route
+holidays.post('/', async(req, res)  => {
+  Holiday.create(req.body, (error, createdHoliday) => {
+    if(error) {
+      res.status(400).json({error: error.message})
+
+    }
+    res.status(200).send(createdHoliday)
+  })
+})
+
+// Delete route
+holidays.delete('/:id', (req, res) => {
+  Holiday.findByIdAndRemove(req.params.id, (err, deletedHoliday) => {
+    if (err) {
+      res.status(400).json({ error: err.message })
+    }
+    res.status(200).json(deletedHoliday)
+  })
+})
+// Update route
+holidays.put('/:id', (req, res) => {
+  Holiday.findByIdAndUpdate(req.params.id, req.body, { new: true }, (err, updatedHoliday) => {
+    if (err) {
+      res.status(400).json({ error: err.message })
+    }
+    res.status(200).json(updatedHoliday)
+  })
+})
+
+module.exports = holidays
