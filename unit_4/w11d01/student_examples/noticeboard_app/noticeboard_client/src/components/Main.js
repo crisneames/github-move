aca341ebd601a7d76d8f778e6d89af7bc09488d4
Cristi Neames <crisneames.gmail.com>
2020-03-31 03:55:25 -0500

    created rails server

--- unit_4/w11d01/student_examples/noticeboard_app/noticeboard_client/src/components/Main.js
+++ unit_4/w11d01/student_examples/noticeboard_app/noticeboard_client/src/components/Main.js
@@ -2,12 +2,16 @@ import React from 'react'
 import Notices from './Notices.js'
 
 function Main(props) {
-  const { notices } = props
-    return (
-      <main>
-        <Notices notices={notices} />
-      </main>
-    )
+  const { notices, handleDelete, handleUpdate } = props
+  return (
+    <main>
+      <Notices
+        notices={notices}
+        handleDelete={handleDelete}
+        handleUpdate={handleUpdate}
+      />
+    </main>
+  )
 }
 
 export default Main
--- unit_4/w11d01/student_examples/noticeboard_app/noticeboard_client/src/components/Notices.js
+++ unit_4/w11d01/student_examples/noticeboard_app/noticeboard_client/src/components/Notices.js
@@ -2,12 +2,19 @@ import React from 'react'
 import Notice from './Notice.js'
 
 function Notices(props) {
-  const { notices } = props
-    return (
-      <div>
-        {notices.map(notice => <Notice key={notice.id} notice={notice}/>)}
-      </div>
-    )
+  const { notices, handleDelete, handleUpdate } = props
+  return (
+    <div>
+      {notices.map(notice => (
+        <Notice
+          key={notice.id}
+          notice={notice}
+          handleDelete={handleDelete}
+          handleUpdate={handleUpdate}
+        />
+      ))}
+    </div>
+  )
 }
 
 export default Notices
